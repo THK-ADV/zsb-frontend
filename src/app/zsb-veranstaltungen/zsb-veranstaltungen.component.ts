66885565f1f66d610fbd242057f530dd8c547291
pschm <philipp.schmeier@gmail.com>
2020-08-20 16:37:05 +0200

    Add veranstaltung component

---
+++ src/app/zsb-veranstaltungen/zsb-veranstaltungen.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-zsb-veranstaltungen',
+  templateUrl: './zsb-veranstaltungen.component.html',
+  styleUrls: ['./zsb-veranstaltungen.component.css']
+})
+export class ZsbVeranstaltungenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
