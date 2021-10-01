32e0b4373814d65d499b1968737f953abd4dd66a
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2021-10-01 23:19:02 +0200

    fix case sensitivity

--- src/app/shared/keywordsearch.ts
+++ src/app/shared/keywordsearch.ts
@@ -1,6 +1,6 @@
-export function buildCustomFilter<T>(concatenatedString: (T) => string): (affiliation: T, filter: string) => boolean {
+export function buildCustomFilter<T>(concatenatedString: (t: T) => string): (affiliation: T, filter: string) => boolean {
   return (affiliation: T, filter: string) => {
-    const keywords = filter.split(',').map(it => it.trim())
+    const keywords = filter.split(',').map(it => it.trim().toLowerCase())
     let keywordsFound = true
     keywords.forEach(keyword => {
       if (!concatenatedString(affiliation).toLowerCase().includes(keyword)) {
