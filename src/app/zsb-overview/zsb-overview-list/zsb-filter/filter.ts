9c60cdac1b4e847797fc246d93752ff5700964cc
Anja Bertels <anja.bertels@th-koeln.de>
2024-02-10 14:10:35 +0100

    add dropdown and checkbox school filters

--- src/app/zsb-overview/zsb-overview-list/zsb-filter/filter.ts
+++ src/app/zsb-overview/zsb-overview-list/zsb-filter/filter.ts
@@ -10,3 +10,84 @@ export class SchoolNameFilter implements Filter {
     return data.filter(item => item.school.name.toLowerCase().includes(schoolName.toLowerCase()))
   }
 }
+
+export class SchoolTypeFilter implements Filter {
+  filter(data: SchoolWithEvents[], criteria: number): SchoolWithEvents[] {
+    const schoolType = criteria
+    return data.filter(item => item.school.type === schoolType)
+  }
+}
+
+export class GovernmentDisctrictFilter implements Filter {
+  filter(data: SchoolWithEvents[], criteria: string): SchoolWithEvents[] {
+    const governmentDistrict = criteria
+    return data.filter(item => item.school.address.city.governmentDistrict === governmentDistrict )
+  }
+}
+
+export class ConstituencyFilter implements Filter {
+  filter(data: SchoolWithEvents[], criteria: string): SchoolWithEvents[] {
+    const constituency = criteria
+    return data.filter(item => item.school.address.city.constituency === constituency )
+  }
+}
+
+export class DesignationFilter implements Filter {
+  filter(data: SchoolWithEvents[], criteria: string): SchoolWithEvents[] {
+    const designation = criteria
+    return data.filter(item => item.school.address.city.designation === designation )
+  }
+}
+
+/*export class AmountStudentsFilter implements Filter {
+  filter(data: SchoolWithEvents[], criteria: number[]): SchoolWithEvents[] {
+    // TODO
+  }
+}*/
+
+export class KAoAFilter implements Filter {
+  filter(data: SchoolWithEvents[], criteria: boolean): SchoolWithEvents[] {
+    const kaoa = criteria
+    if (kaoa === false) {
+      return data.filter(item => item.school.kaoaSupervisor === 0)
+    } else {
+      return data.filter(item => item.school.kaoaSupervisor > 0)
+    }
+  }
+}
+
+export class TalentscoutingFilter implements Filter {
+  filter(data: SchoolWithEvents[], criteria: boolean): SchoolWithEvents[] {
+    const talentscout = criteria
+    if (talentscout === false) {
+      return data.filter(item => item.school.talentscout === 0)
+    } else {
+      return data.filter(item => item.school.talentscout > 0)
+    }
+  }
+}
+
+export class CooperationFilter implements Filter {
+  filter(data: SchoolWithEvents[], criteria: boolean): SchoolWithEvents[] {
+    const cooperationcontract = criteria
+    return data.filter(item => item.school.cooperationcontract === cooperationcontract)
+  }
+}
+
+export class CompositeFilter implements Filter {
+  private filters: Filter[]
+
+  constructor(filters: Filter[]) {
+    this.filters = filters
+  }
+
+  filter(data: SchoolWithEvents[], criteria: any): SchoolWithEvents[] {
+    let filteredData = [...data]
+
+    for (const filter of this.filters) {
+      filteredData = filter.filter(filteredData, criteria)
+    }
+
+    return filteredData
+  }
+}
--- src/app/zsb-overview/zsb-overview-list/zsb-filter/zsb-filter.component.ts
+++ src/app/zsb-overview/zsb-overview-list/zsb-filter/zsb-filter.component.ts
@@ -1,7 +1,20 @@
 import {Component, Inject} from '@angular/core'
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog'
-import {SchoolNameFilter} from './filter'
+import {
+  CompositeFilter,
+  ConstituencyFilter, CooperationFilter,
+  DesignationFilter,
+  GovernmentDisctrictFilter, KAoAFilter,
+  SchoolNameFilter,
+  SchoolTypeFilter, TalentscoutingFilter
+} from './filter'
 import {SchoolWithEvents} from '../zsb-overview-list.component'
+import {SchoolType} from '../../../zsb-school/schoolType'
+import {DatabaseService} from '../../../shared/database.service'
+import {Observable} from 'rxjs'
+import {filterDuplicates, filterOptions} from '../../../shared/functions'
+import {Address} from '../../../zsb-address/address'
+import {AddressService} from '../../../shared/address.service'
 
 @Component({
   selector: 'app-zsb-filter',
@@ -10,23 +23,84 @@ import {SchoolWithEvents} from '../zsb-overview-list.component'
 })
 export class ZsbFilterComponent {
   schoolNameChecked = false
+  schoolTypeChecked = false
+  governmentDistrictChecked = false
+  constituencyChecked = false
+  designationChecked = false
   schoolName = ''
+  schoolTypes: Observable<SchoolType[]>
+  addressesObservable: Observable<Address[]>
+  addresses: Address[]
+  governmentDistricts: Observable<string[]>
+  constituencies: Observable<string[]>
+  designations: Observable<string[]>
   schoolWithEvents: SchoolWithEvents[]
+  selectedTypeId: number
+  governmentDistrict: string
+  constituency: string
+  designation: string
+  kaoa: boolean
+  talentscouting: boolean
+  cooperation: boolean
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<ZsbFilterComponent>) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
+              public dialogRef: MatDialogRef<ZsbFilterComponent>,
+              private dbService: DatabaseService,
+              public service: AddressService,
+  ) {
     this.schoolWithEvents = data.schoolWithEvents
     console.log(this.schoolWithEvents)
   }
 
-  onInputChange() {
+  ngOnInit(): void {
+    const controls = this.service.formGroup.controls
+    this.schoolTypes = this.dbService.getSchoolType()
+    this.addressesObservable = this.dbService.getAddressesAtomic()
+    this.addressesObservable.subscribe(addresses => this.addresses = addresses)
+    this.addressesObservable.subscribe(adressen => {
+      // get all values and filter duplicates
+      this.governmentDistricts = filterOptions(controls.governmentDistrict,
+        filterDuplicates(adressen.map(it => it.city.governmentDistrict.trim())))
+      this.constituencies = filterOptions(controls.governmentDistrict,
+        filterDuplicates(adressen.map(it => it.city.constituency.trim())))
+      this.designations = filterOptions(controls.governmentDistrict,
+        filterDuplicates(adressen.map(it => it.city.designation.trim())))
+      console.log(this.governmentDistricts)
+    })
+  }
+
+  onSchoolNameChange() {
     if (this.schoolName.trim() !== '') {
       this.schoolNameChecked = true
     }
   }
 
+  onSchoolTypeChange() {
+    /*if (this.type.trim() !== '') {
+      this.schoolNameChecked = true
+    }*/
+  }
+
+  onGovernmentDistrictChange() { }
+
+  onConstituencyChange() { }
+
+  onDesignationChange() { }
+
   onSubmit() {
     const schoolNameFilter = new SchoolNameFilter()
-    const result = schoolNameFilter.filter(this.schoolWithEvents, this.schoolName)
+    const schoolTypeFilter = new SchoolTypeFilter()
+    const governmentDistrictFilter = new GovernmentDisctrictFilter()
+    const constituencyFilter = new ConstituencyFilter()
+    const designationFilter = new DesignationFilter()
+    const kaoaFilter = new KAoAFilter()
+    const talentscoutingFilter = new TalentscoutingFilter()
+    const cooperationFilter = new CooperationFilter()
+    const compositeFilter = new CompositeFilter([schoolNameFilter, schoolTypeFilter])
+    // const result = compositeFilter.filter(this.schoolWithEvents, filterCriteria)
+    console.log('this.kaoa')
+    console.log(this.talentscouting)
+    const result = talentscoutingFilter.filter(this.schoolWithEvents, this.talentscouting)
     console.log(result)
     this.dialogRef.close(result)
   }
